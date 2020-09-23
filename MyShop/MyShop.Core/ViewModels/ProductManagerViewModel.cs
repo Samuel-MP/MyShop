946a952cef8edaea3ed2ba63f830212c256cf052
Samuel MP <samuelmp1919@gmail.com>
2020-09-23 15:36:21 -0600

    Product category adding

---
+++ MyShop/MyShop.Core/ViewModels/ProductManagerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+    public class ProductManagerViewModel
+    {
+        public Product Product { get; set; } // objeto
+        public IEnumerable <ProductCategory> ProductCategories { get; set; }
+        //lista que podemos iterar a través de nuestra product category
+
+    }
+}
